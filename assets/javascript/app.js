a8ba269d4adef9b5be34f7fe0688396c3b900c58
Jill Morimoto <jillmorimoto@gmail.com>
2018-07-05 23:01:51 -0700

    2

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+var triviaQuestions = [{
+    
+    question: "The Federalist Papers supported the passage of the U.S. Constitution. Name one of the writers.",
+    answerList: ["Thomas Jefferson", "Hercules Mulligan", "Alexander Hamilton", "Phillip Schuyler"],
+    answer: 2
+
+},{
+
+    question: "Who did the United States fight in World War II?",
+    answerList: ["North Korea, Iran, Iraq", "Japan, Germany, Italy", "Mexico, Canada, China", "Syria, Iran, Libya"],
+    answer: 1
+
+},{
+
+    question: "Name one U.S. Territory",
+    answerList: ["Cuba", "Hawaii", "Falklands", "Puerto Rico"],
+    answer: 3
+},{
+
+    question: "The idea of self-government is in the first three words of the Constitution. What are these words?",
+    answerList: ["We the people", "Four-score and", "When in the", "We hold these"],
+    answer: 0
+},{
+
+    question: "How many amendments does the Constitution have?",
+    answerList: ["23", "42", "60", "27"],
+    answer: 3
+},{
+
+    question: "The House of Representatives has how many voting members?",
+    answerList: ["100", "538", "435", "381"],
+    answer: 2
+},{
+
+    question: "When was the Constitution written?",
+    answerList: ["1787", "1781", "1776", "1784"],
+    answer: 0
+},{
+
+    question: "Who was President during World War I?",
+    answerList: ["Calvin Coolidge", "Woodrow Wilson", "Warren G. Harding", "William Howard Taft"],
+    answer: 1
+},{
+
+    question: "What is the name of the national anthem?",
+    answerList:["God Bless America", "Party in the USA", "The Star-Spangled Banner", "America F*** Yeah"],
+    answer: 2
+},{
+
+    question: "During the Cold War, what was the main concern of the United States?",
+    answerList: ["Democracy", "Capitalism", "Freedom", "Communism"],
+    answer: 3
+},{
+
+    question: "What is the “rule of law”?",
+    answerList: ["No one is above the law", "The President is exempt", "The judiciary", "Police use of force"],
+    answer: 0
+},{
+
+    question: "What do we call the first ten amendments to the Constitution?",
+    answerList: ["Ten Crack Commandments", "The Bill of Rights", "Listicle", "EMILY's List"],
+    answer: 1
+},{
+
+    question: "What does the President’s Cabinet do?",
+    answerList: ["Hold secret meetings", "Advise the President", "Email", "Twitter"],
+    answer: 1
